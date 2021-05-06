37cc367e0a168ff71666f95897cf434d878fb916
Iv Po <me@dotdev.ru>
2021-05-06 13:27:48 +0300

    Fixes

--- packages/inversify/README.md
+++ packages/inversify/README.md
@@ -5,3 +5,11 @@ This library was generated with [Nx](https://nx.dev).
 ## Running unit tests
 
 Run `nx test inversify` to execute the unit tests via [Jest](https://jestjs.io).
+
+## Install
+
+```js
+import { Container, InversifyProvider } from '@reactpkg/inversify';
+
+<InversifyProvider container={Container}></InversifyProvider>
+```
--- packages/reactapp/core/src/modules/react-router/route-collection.service.ts
+++ packages/reactapp/core/src/modules/react-router/route-collection.service.ts
@@ -1,6 +1,7 @@
 // import { flattenDeep } from 'lodash';
-import { RouteProps } from 'react-router';
+// import { RouteProps } from 'react-router';
 import { Inject, Injectable, InversifyContainer } from '@reactpkg/inversify';
+import { RouteConfig } from 'react-router-config';
 
 // import { ROUTES_TOKEN } from './react-router.constants';
 
@@ -9,8 +10,8 @@ export class RouteCollectionService {
   @Inject(InversifyContainer)
   private container: InversifyContainer;
 
-  getRoutes(isLogged = true): RouteProps[] {
-    const routes: RouteProps[] = [];
+  getRoutes(isLogged = true): RouteConfig[] {
+    const routes: RouteConfig[] = [];
     // const tagName = isLogged ? ROUTES_TOKEN : ROUTES_TOKEN; // CoreService.AUTH_ROUTES;
     try {
       // routes = [
