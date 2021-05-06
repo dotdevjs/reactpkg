9100c21eaab3c7de24fb5511d6ad6145fba3710b
Iv Po <me@dotdev.ru>
2021-05-06 13:51:30 +0300

    fix(reactapp/core)

--- packages/reactapp/core/src/modules/react-router/react-router.module.ts
+++ packages/reactapp/core/src/modules/react-router/react-router.module.ts
@@ -1,11 +1,28 @@
 import { ContainerModule, interfaces } from 'inversify';
+import { RouteConfig } from 'react-router-config';
 
 import { ROUTES_TOKEN } from './react-router.constants';
 import { RouteCollectionService } from './route-collection.service';
 
-export const ReactRouterModule = new ContainerModule(
-  (bind: interfaces.Bind, unbind: interfaces.Unbind) => {
-    bind(ROUTES_TOKEN).toConstantValue([]);
-    bind(RouteCollectionService).to(RouteCollectionService);
-  }
-);
+export interface ReactRouterModuleFactoryProps {
+  routes?: RouteConfig[];
+}
+export const ReactRouterModuleFactory = (
+  props: ReactRouterModuleFactoryProps = {}
+): ContainerModule => {
+  return new ContainerModule(
+    (bind: interfaces.Bind, unbind: interfaces.Unbind) => {
+      bind(ROUTES_TOKEN).toConstantValue(props.routes || []);
+      bind(RouteCollectionService).to(RouteCollectionService);
+    }
+  );
+};
+
+export const ReactRouterModule = ReactRouterModuleFactory();
+
+//  export const ReactRouterModule = new ContainerModule(
+//    (bind: interfaces.Bind, unbind: interfaces.Unbind) => {
+//      bind(ROUTES_TOKEN).toConstantValue([]);
+//      bind(RouteCollectionService).to(RouteCollectionService);
+//    }
+//  );
