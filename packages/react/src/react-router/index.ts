235a465d1df10516eee64bc9badf2d136b9793c7
Iv Po <me@dotdev.ru>
2022-07-03 21:48:50 +0300

    fixes

--- packages/react/src/react-router/index.ts
+++ packages/react/src/react-router/index.ts
@@ -1,3 +1,5 @@
+import { RouteConfig } from 'react-router-config';
+
 export * from './render-routes';
 export * from './route-collection';
 export * from './react-router.module';
@@ -5,3 +7,5 @@ export * from './react-router.module';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { formatRoute } = require('react-router-named-routes');
 export { formatRoute };
+
+export type Routes = RouteConfig[];
